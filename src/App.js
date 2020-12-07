be4054cc54f8e6f6cc3abfdc21c4828036278b99
markdenardo <mark.denardo@gmail.com>
2020-12-06 22:40:11 -0500

    game of life sketch added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch'
+import golsketch from './gol-sketch';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <>
           <P5Wrapper sketch={sketch} />
+          <P5Wrapper sketch={golsketch} />
 
         </>
       </header>
